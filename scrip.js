61ecaf2ed86a638838a02d1d76763d39502fe6cb
jhirarh <jhirarh.cavalcante@uaubox.com.br>
2022-04-05 12:15:41 -0300

    Folha ao enivar nome e email

---
+++ scrip.js
@@ -0,0 +1,22 @@
+function saveExcel(event){
+  event.preventDefault()
+
+  let name = document.getElementById("name");
+
+  let email = document.getElementById("email");
+
+  let url = "https://script.google.com/macros/s/AKfycbwJScFseAiwEOGUbDnN8AaqWHCiA7gmhjOey1l_xP0RcxujUB6x/exec?"
+  
+
+  fetch(url + new URLSearchParams({
+  name,
+  email}), {method: 'GET'})
+    .then(response => {
+        response.json()
+            .then(data => {
+                console.log(data)
+            })
+    })
+
+
+}
